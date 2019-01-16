10dd38a461538def9642a4ab708b7f7393458970
Taimuraz <Taimuraz.Bitakov@kronshtadt.ru>
2019-01-16 15:50:05 +0300

    __+

--- person.py
+++ person.py
@@ -4,29 +4,45 @@ class Person:
         self.job = job
         self.pay = pay
 
-
     def lastName(self):
         return self.name.split()[-1]
 
     def giveRaise(self, percent):
-        self.pay = int(self.pay*(1 + percent))
+        self.pay = int(self.pay * (1 + percent))
+        print("giveRaise from Developer.")
 
     def __str__(self):
         return '[Person: %s, %s]' % (self.name, self.pay)
 
 
 class Manager(Person):
+    def __init__(self, name, pay):
+        Person.__init__(self, name, 'manager', pay)
 
+    def __str__(self):
+        return '[Manager: %s, %s]' % (self.name, self.pay)
+
+    def giveRaise(self, percent, bonus=.10):
+        print("giveRaise from Manager.")
+        Person.giveRaise(self, percent + bonus)
 
 
 if __name__ == '__main__':
     bob = Person('Bob Smith')
-    sue = Person('Sue Jones', job='Developer', pay=100000)
+    sue = Person('Sue Jones', job='developer', pay=100000)
 
     bob.giveRaise(0.5)
     sue.giveRaise(0.5)
 
     print(bob)
-    print(sue.__repr__())
+    print(sue)
 
+    tom = Manager('Tom Jones', 50000)
+    tom.giveRaise(.10)
+    print(tom.lastName())
+    print(tom)
 
+    print("--All three--")
+    for object in (bob, sue, tom):
+        object.giveRaise(.10)
+        print(object)
